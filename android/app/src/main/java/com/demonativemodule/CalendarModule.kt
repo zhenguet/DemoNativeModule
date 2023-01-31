6c7948357771dbf0f4d9a2ccc585e4c4df82d820
Zheng UET <zhenguet@gmail.com>
2023-01-31 08:56:02 +0700

    add CalendarModule

---
+++ android/app/src/main/java/com/demonativemodule/CalendarModule.kt
@@ -0,0 +1,18 @@
+package com.demonativemodule
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import android.util.Log
+
+class CalendarModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    override fun getName() = "CalendarModule"
+
+    @ReactMethod
+    fun createCalendarEvent(name: String, location: String) {
+        Log.d("CalendarModule", "Create event called with name: $name and location: $location")
+    }
+}
+
+
